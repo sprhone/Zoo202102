0c63886ab5e976853ddd4d359b2591ef63adc49f
rchinhamhora <rchinhamhora@gmail.com>
2021-01-24 14:59:35 -0600

    Commit Horse.

--- src/AnimalNoise.java
+++ src/AnimalNoise.java
@@ -1,10 +1,4 @@
-import model.Cat;
-import model.Cow;
-import model.Dog;
-import model.Pig;
-import model.Duck;
-import model.Mongoose;
-import model.Flamingo;
+import model.Horse;
 
 public class AnimalNoise {
 
@@ -12,27 +6,7 @@ public class AnimalNoise {
 	// only push this file and the animal class you created
 
 	public static void main(String[] args) {
-
-		Cat fluffy = new Cat();
-		System.out.println(fluffy.speak());
-		
-		Dog doggy = new Dog();
-		System.out.println(doggy.speak());
-		
-		Pig wilbur = new Pig();
-		System.out.println(wilbur.speak());
-
-		Cow moomoo = new Cow();
-		System.out.println(moomoo.speak());
-		
-		Duck ducky = new Duck();
-		System.out.println(ducky.speak());
-		
-		Mongoose rikkitikkitavi = new Mongoose();
-		System.out.println(rikkitikkitavi.speak());
-		
-		Flamingo Ricco = new Flamingo();
-		System.out.println(Ricco.speak());
+		Horse horse = new Horse();
+		System.out.println(horse.speak());
 	}
 }
-
