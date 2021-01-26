d79ddcb162e739bb78a1fcf8ddeaca1f9a9c2304
jjwood1 <61518642+jjwood1@users.noreply.github.com>
2021-01-26 17:18:52 -0600

    Add files via upload

--- src/AnimalNoise.java
+++ src/AnimalNoise.java
@@ -12,6 +12,7 @@ import model.Owl;
 import model.Chinchilla;
 import model.Horse;
 import model.Lion;
+import model.Axolotl;
 
 public class AnimalNoise {
 
@@ -56,12 +57,15 @@ public class AnimalNoise {
 		Horse horse = new Horse();
 		System.out.println(horse.speak());
 		
-
 		Crow crow = new Crow();
 		System.out.println(crow.speak());
 
 		Lion lion = new Lion(); 
 		System.out.println(lion.speak());
+		
+		Axolotl Axo = new Axolotl();
+		System.out.println(Axo.speak());
+		
 		
 
 	}
